4650ce915768c28f6488d207b810451080d38ac2
Ismati5 <79214939+Ismati5@users.noreply.github.com>
2022-03-15 13:12:02 +0100

    Iniciado servidor TCP

--- src/backend.java
+++ src/backend.java
@@ -1,5 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+ 
+/**
+ * Programa principal del servidor central
+ *
+ * @author MoonCode
+ */
 public class backend {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+ 
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 1) {
+            System.out.println("Error sintactico. Usa backend (port)");
+            return;
+        }
+ 
+        int port = Integer.parseInt(args[0]);
+ 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Servidor central escuchando en el puerto " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Cliente conectado");
+ 
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+ 
+                writer.println(new Date().toString());
+                socket.close();
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 }
