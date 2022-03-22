177312315f41c79e172184bb4578e161e2ac05a3
Ismati5 <79214939+Ismati5@users.noreply.github.com>
2022-03-22 15:47:38 +0100

    Iniciado logica del juego

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+import enums.State;
+
+public class Game{
+    
+    public String gameId;
+    public State gameState;
+    public ArrayList<Player> gamePlayers = new ArrayList<Player>();
+
+}
+
---
+++ src/gameManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Game Manager
+ *
+ * @author MoonCode
+ */
+public class gameManager{
+
+    public ArrayList<Game> games = new ArrayList<Game>();
+
+}
