7e049163c34ddfc97c07b82de7b01d00700b7931
PilarEster <77052745+PilarEster@users.noreply.github.com>
2022-03-30 17:57:02 +0200

    Update createtables.sql

--- database/createtables.sql
+++ database/createtables.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Player(
-    Username varchar(30) PRIMARY KEY,
+    Username varchar(40) PRIMARY KEY,
     Password_hash varchar(30),
-    Image_ID integer,
+    Email varchar(40) unique,
+    Image_ID integer unique,
     Mooncoins integer,
     Stars integer
 );
@@ -22,6 +23,8 @@ CREATE TABLE Story(
 CREATE TABLE Participant(
     Username varchar(30),
     Story_ID integer,
+    Voted varchar(30) unique,
+    Creator boolean,
     PRIMARY KEY (Username, Story_ID),
     FOREIGN KEY (Username) REFERENCES Player(Username),
     FOREIGN KEY (Story_ID) REFERENCES Story(Story_ID)
@@ -29,24 +32,25 @@ CREATE TABLE Participant(
 
 CREATE TABLE Tale_mode(
     Story_ID integer PRIMARY KEY,
-    Max_turns integer,
-    Max_paragraph_chars integer,
-    Max_write_time real,
+    Max_turns integer unique,
+    Max_paragraph_chars integer unique,
+    Max_write_time real unique,
     Privacy boolean,                -- 1=private, 0=public
-    Finished boolean,               -- 1=finished, 0=no finished
-    Scored real,
+    Finished boolean,               -- 1=finished, 0=not finished
+    Scored boolean,                 -- 1=scored, 0=not scored
     Title varchar(50),
     FOREIGN KEY (Story_ID) REFERENCES Story(Story_ID)
 ) INHERITS (Story);
 
 CREATE TABLE Quick_match(
     Story_ID integer PRIMARY KEY,
+    Mode integer,                   --0=normal, 1=tendencias Twitter, 2=palabras obligatorias
     FOREIGN KEY (Story_ID) REFERENCES Story(Story_ID)
 ) INHERITS (Story);
 
 CREATE TABLE Paragraph(
     Text varchar(250),
-    Score real,
+    Score real unique,
     Turn_number integer,
     Username varchar(30),
     Story_ID integer,
