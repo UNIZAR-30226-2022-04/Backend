0253991b05e6eeb52407899969cc0fd7be7d82ef
Jorg403 <76389552+Jorg403@users.noreply.github.com>
2022-03-30 17:40:39 +0200

    mejora de login y register y añadido comentarios

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,11 +4,23 @@ import users from "../../lib/users"
 export default function handler(req, res) {
     const mensaje = req.body;
     const user = users.find(user => user.username == mensaje.username)
-    const pending = 3   //cantidad de notificaciones
 
     if (user != null){
         if (user.password == mensaje.password){
-            res.status(200).json({ result:'success',reason:'' , notifications: pending});
+            const notifications = 3   // amount of notifications (DB NEEDED)
+            const coins = 403   // amount of coins (DB NEEDED)
+            const bestFour = [  // top 4 friends with the higest score (DB NEEDED)
+                {"username": users[4].username, "score": 2706},
+                {"username": users[1].username, "score": 2048},
+                {"username": users[2].username, "score": 1414},
+                {"username": users[5].username, "score": 1024}
+            ]
+            const picture = 7 // ID number of the profile picture (DB NEEDED)
+            res.status(200).json({  result:'success',
+                                    notifications: notifications,
+                                    coins: coins,
+                                    bestFour: bestFour,
+                                    picture: picture});
         } else {
             res.status(200).json({ result:'error',reason:'wrong_password'});
         }
--- pages/api/register.js
+++ pages/api/register.js
@@ -6,19 +6,35 @@ import users from "../../lib/users"
 export default function handler(req, res) {
     const mensaje = req.body;
     const user = users.find(user => user.username == mensaje.username);
-    const pending = 3   //cantidad de notificaciones
 
     if (user == null){
-        const salt = crypto.randomBytes(16).toString("hex")
+        const notifications = 0   // amount of notifications (HARDCODED)
+        const coins = 100   // amount of initial coins (HARCODED)
+        const bestFour = [  // top 4 friends with the higest score (NO SENSE HERE)
+            {"username": "null"},
+            {"username": "null"},
+            {"username": "null"},
+            {"username": "null"}
+        ]
+        const picture = 0 // ID number of the default profile picture (HARDCODED)
+            
+        //const salt = crypto.randomBytes(16).toString("hex")
 
         const query = {
             username: mensaje.username,
-            salt: salt,
+            //salt: salt,
             password: mensaje.password,
             //password: CryptoJS.SHA512(salt + mensaje.password).toString(),
         }
         users.push(query);
-        res.status(200).json({ result:'success',reason:'', notifications: pending});
+
+        // STORE VALUES IN DB HERE
+
+        res.status(200).json({  result:'success',
+                                notifications: notifications,
+                                coins: coins,
+                                bestFour: bestFour,
+                                picture: picture});
     } else {
         res.status(200).json({ result:'error',reason:'user_already_registered'});
     }
