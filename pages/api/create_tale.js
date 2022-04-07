f74685146c2b4aac6919557db6c6a4179e622563
Ismati5 <79214939+Ismati5@users.noreply.github.com>
2022-04-08 01:03:28 +0200

    + Creadas DAO tale_mode, story y paragraph.
    + Modificado restricciones de unico en BBDD.
    
    + Añadida API create_tale.
    
    + Eliminado Herobrine.

---
+++ pages/api/create_tale.js
@@ -0,0 +1,40 @@
+import { createParagraphDB } from "../../prisma/queries/CREATE/paragraph";
+import { createStoryDB } from "../../prisma/queries/CREATE/story";
+import { createTaleDB } from "../../prisma/queries/CREATE/tale_mode";
+import { selectPlayerDB } from "../../prisma/queries/SELECT/player";
+
+// Al ir a http://localhost:3000/api/create_tale te devuelve el siguiente json
+export default async (req, res) => {
+	const message = req.body;
+
+	const user = await selectPlayerDB(message.username);
+
+	// checks if username exists
+	if (user != undefined) {
+		if (user.password_hash == message.password) {
+			const story = await createStoryDB();
+
+			const dataTale = {
+				story_id: story.story_id,
+				max_turns: message.maxTurns,
+				max_paragraph_chars: message.maxCharacters,
+				privacy: message.privacy,
+				title: message.title,
+			};
+			await createTaleDB(dataTale);
+
+			const dataParagraph = {
+				text: message.first_paragraph,
+				username: message.username,
+				story_id: story.story_id,
+			};
+			await createParagraphDB(dataParagraph);
+
+			res.status(200).json({ result: "success", reason: "" });
+		} else {
+			res.status(200).json({ result: "error", reason: "wrong_password" });
+		}
+	} else {
+		res.status(200).json({ result: "error", reason: "user_not_found" });
+	}
+};
