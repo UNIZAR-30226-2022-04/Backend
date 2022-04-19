f4ac8347193573b0ee74c7ace7721b889565cf7d
Ismati5 <79214939+Ismati5@users.noreply.github.com>
2022-04-19 02:05:18 +0200

    - Iniciado quick_game   -> API create_room   -> API join_room   -> Creada lista de partidas en memoria

---
+++ pages/api/quick_game/create_room.js
@@ -0,0 +1,34 @@
+import { createGame } from "../../../lib/Game";
+import Player from "../../../lib/Player";
+import { selectPlayerDB } from "../../../prisma/queries/SELECT/player";
+
+// Al ir a http://localhost:3000/api/quick_game/create_room te devuelve el siguiente json
+export default async (req, res) => {
+	const message = req.body;
+
+	const user = await selectPlayerDB(message.username);
+
+	// checks if username exists
+	if (user != undefined) {
+		if (user.password_hash == message.password) {
+			const p = new Player(
+				message.username,
+				message.password,
+				user.image_ID,
+				user.stars,
+				user.mooncoins
+			);
+
+			var id =
+				"#" +
+				Date.now().toString(36).substr(12, 4) +
+				Math.random().toString(36).substr(2, 5);
+			createGame(id, p, message.time, message.isPrivate, message.mode);
+			res.status(200).json({ result: "success", id: id });
+		} else {
+			res.status(200).json({ result: "error", reason: "wrong_password" });
+		}
+	} else {
+		res.status(200).json({ result: "error", reason: "user_not_found" });
+	}
+};
