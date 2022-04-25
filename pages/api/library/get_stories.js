c9b35df184be5b6b250f793bf5c295c91b227e9c
Jorg403 <76389552+Jorg403@users.noreply.github.com>
2022-04-25 04:22:40 +0200

    creado get_stories y debugeado
    Encontrados errores a la hora de crear tales y párrafos. Seguramente haya que tirar la tabla de stories y crearla de nuevo con el autoincremento

--- pages/api/library/get_stories.js
+++ pages/api/library/get_stories.js
@@ -1,7 +1,8 @@
 import { selectPlayerDB } from "../../../prisma/queries/SELECT/player";
+import { selectStoriesDB } from "../../../prisma/queries/SELECT/stories";
 import {checkFields} from "../../../lib/checkFields";
 
-// Al ir a http://localhost:3000/api/login te devuelve el siguiente json
+// Al ir a http://localhost:3000/api/get_stories te devuelve el siguiente json
 export default async (req, res) => {
 	const message = req.body;
 	
@@ -14,12 +15,17 @@ export default async (req, res) => {
 
 	const user = await selectPlayerDB(message.username);
 
+	if (message.username.length==0||message.password.length==0){
+		res.status(200).json({ result: "error", reason: "invalid credentials" });
+	}
+
 	// checks if the requested user exists
 	if (user != undefined) {
 		// checks password
 		if (user.password_hash == message.password) {
 			//cambiar por password + anadir mecanismo hash
-			res.status(200).json({ result: "success", reason: "" });
+            const stories = selectStoriesDB(message.username);
+			res.status(200).json({ result: "success", stories, reason: "" });
 		} else {
 			res.status(200).json({ result: "error", reason: "wrong_password" });
 		}
---
+++ prisma/queries/SELECT/stories.js
@@ -0,0 +1,28 @@
+import prisma from "../../../lib/prisma";
+
+export async function selectStoriesDB(username) {
+	const query = await prisma.participant.findMany({
+		where: {
+			username: { equals: username },
+		},
+        select: {
+            story: {
+                select: {
+                    story_id: true,
+                    tale: {
+                        select: {
+                            title: true,
+                        },
+                    },
+                    quick_match: {
+                        select: {
+                            mode: true,
+                        },
+                    },
+                },
+            },
+        },
+	});
+
+	return query;
+}
