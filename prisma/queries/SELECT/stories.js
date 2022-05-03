742c4f9d2719a613560c7e7eeaa4496ba4786a3e
Ismati5 <79214939+Ismati5@users.noreply.github.com>
2022-05-03 17:43:07 +0200

    Bug fix get_stories

--- prisma/queries/SELECT/stories.js
+++ prisma/queries/SELECT/stories.js
@@ -5,20 +5,20 @@ export async function selectStoriesDB(username) {
 		where: {
 			username: { equals: username },
 		},
-        select: {
-            story: {
-                select: {
-                    story_id: true,
-                    date: true,
-                    tale: true,
-                    quick_match: {
-                        select: {
-                            mode: true,
-                        },
-                    },
-                },
-            },
-        },
+		select: {
+			story: {
+				select: {
+					story_id: true,
+					date: true,
+					tale: true,
+					quick_match: {
+						select: {
+							mode: true,
+						},
+					},
+				},
+			},
+		},
 	});
 	return query;
 }
